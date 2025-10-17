88851db735e03e7fbc9296363b2877de915c3ea6
ScPro <muhamadievbogdan@gmail.com>
2025-10-18 03:41:38 +0500

    configure base project structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Home } from "./pages/Home";
+
+document.documentElement.setAttribute("data-theme", "dark")
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
