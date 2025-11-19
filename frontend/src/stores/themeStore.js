2c549e2a4a80901920ae583f7c72e5509b84a9c5
ScPro <muhamadievbogdan@gmai.com>
2025-11-19 21:13:10 +0500

    change in themeStore

--- frontend/src/stores/themeStore.js
+++ frontend/src/stores/themeStore.js
@@ -12,7 +12,6 @@ export const useThemeStore = create((set) => ({
     currentTheme: getInitialTheme(),
     toggleTheme: () => set((state) => {
         const newTheme = state.currentTheme === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
         localStorage.setItem('theme', newTheme);
         return { currentTheme: newTheme };
     }),
