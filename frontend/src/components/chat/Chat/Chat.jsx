a001f762e8ee905b74102e26490b26c7fafad4fe
ScPro <muhamadievbogdan@gmail.com>
2025-12-17 17:49:02 +0500

    remove messages template

--- frontend/src/components/chat/Chat/Chat.jsx
+++ frontend/src/components/chat/Chat/Chat.jsx
@@ -1,30 +1,10 @@
 import styles from "./Chat.module.css"
 import { InputField } from "../InputField"
-import { MessageFromUser } from "../Message"
-import { MessageFromAssistant } from "../Message"
-import { MESSAGES } from "./mt"
 
 export function Chat(){
-    const messages = MESSAGES.map((item, index) => {
-        if (item.speaker === "User") {
-            return (
-                <MessageFromUser key={index}>
-                    {item.text}
-                </MessageFromUser>
-            )
-        } else {
-            return (
-                <MessageFromAssistant key={index}>
-                    {item.text}
-                </MessageFromAssistant>
-            )
-        }
-    });
-
     return (
         <div className={styles.chat}>
             <div className={styles.messages_wrapper}>
-                {messages}
             </div>
             <div className={styles.input_field_wrapper}>
                 <InputField />
