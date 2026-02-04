58f4bbdb8030cda6592739e8269219f5155fffde
ScPro <muhamadievbogdan@gmail.com>
2026-02-04 18:57:54 +0500

    rename api router prefix

--- backend/src/api/v1/router.py
+++ backend/src/api/v1/router.py
@@ -4,7 +4,7 @@ from api.v1.endpoints.auth import router as auth_router
 from api.v1.endpoints.chats import router as chat_router
 from api.v1.endpoints.messages import router as message_router
 
-router = APIRouter(prefix="/api")
+router = APIRouter(prefix="/api/v1")
 
 router.include_router(auth_router)
 router.include_router(chat_router)
