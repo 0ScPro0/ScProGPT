075ae782939b23ca634ce04d9fc9efec63a29a7e
ScPro <muhamadievbogdan@gmail.com>
2026-02-04 05:39:19 +0500

    include routers

--- backend/src/api/v1/router.py
+++ backend/src/api/v1/router.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
-from core.config import settings
+from .endpoints.auth import router as auth_router
+from .endpoints.chats import router as chat_router
+from .endpoints.messages import router as message_router
 
 router = APIRouter(prefix="/api")
+
+router.include_router(auth_router)
+router.include_router(chat_router)
+router.include_router(message_router)
 
 @router.get("/")
 def read_root():
