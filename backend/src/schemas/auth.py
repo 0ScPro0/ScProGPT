1527090857f755fb81e5fff014a5cbc1a980a7c7
ScPro <muhamadievbogdan@gmail.com>
2026-02-07 03:09:48 +0500

    add security.py, update auth schema

--- backend/src/schemas/auth.py
+++ backend/src/schemas/auth.py
@@ -1,16 +1,23 @@
+from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
+from src.database.models.user import User
 from src.schemas.user import UserCreate
 
 class Token(BaseModel):
     access_token: str
-    token_type: str = "bearer"
+    token_type: str = "jwt"
     expires_in: Optional[int] = None
 
-class LoginRequest(BaseModel):
+class SignInRequest(BaseModel):
     email: EmailStr
     password: str
 
-class RegisterRequest(UserCreate):
+class SignUpRequest(UserCreate):
     pass
+
+class SignInResponse(BaseModel):
+    access_token: str
+    expiration: datetime
+    user_info: User
