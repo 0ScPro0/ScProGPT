9ecc69cb0e2ca3787d774ebe60f4c7761b428476
ScPro <muhamadievbogdan@gmail.com>
2026-03-20 02:25:13 +0500

    fix imports

--- backend/src/api/v1/endpoints/__init__.py
+++ backend/src/api/v1/endpoints/__init__.py
@@ -2,12 +2,10 @@ from api.v1.endpoints.auth import router as auth_router
 from api.v1.endpoints.users import router as user_router
 from api.v1.endpoints.chats import router as chat_router
 from api.v1.endpoints.messages.messages import router as message_router
-from api.v1.endpoints.ai import router as ai_router
 
 __all__ = [
     "message_router",
     "user_router",
     "chat_router",
     "auth_router",
-    "ai_router",
 ]
