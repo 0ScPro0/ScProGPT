9478ed1784a4d90aa69013ffc2929f7350cc3d33
ScPro <muhamadievbogdan@gmail.com>
2026-03-25 16:00:43 +0500

    change in dependencies

--- backend/src/api/dependencies.py
+++ backend/src/api/dependencies.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import database, User
+from database import database, User, chat_crud, message_crud, user_crud
 from schemas.auth import (
     SignInRequest,
     SignUpRequest,
@@ -14,6 +14,8 @@ from schemas.auth import (
 )
 from schemas.user import UserSchema, UserResponse
 from services.auth import AuthService
+from services.chat import ChatService
+from services.message import MessageService
 from services.user import UserService
 from services.ai.service import AIService
 
@@ -32,19 +34,35 @@ async def get_auth_service(
     session: AsyncSession = Depends(get_db_session),
 ) -> AuthService:
     """Get auth service"""
-    return AuthService(session)
+    return AuthService(session, user_crud)
 
 
 async def get_user_service(
     session: AsyncSession = Depends(get_db_session),
 ) -> UserService:
     """Get user service"""
-    return UserService(session)
+    return UserService(session, user_crud)
 
 
-async def get_ai_service() -> AIService:
+async def get_chat_service(
+    session: AsyncSession = Depends(get_db_session),
+) -> ChatService:
+    """Get chat service"""
+    return ChatService(session, chat_crud)
+
+
+async def get_message_service(
+    session: AsyncSession = Depends(get_db_session),
+) -> MessageService:
+    """Get message service"""
+    return MessageService(session, message_crud)
+
+
+async def get_ai_service(
+    session: AsyncSession = Depends(get_db_session),
+) -> AIService:
     """Get ai service"""
-    return AIService()
+    return AIService(session)
 
 
 __all__ = [
@@ -63,5 +81,7 @@ __all__ = [
     "get_db_session",
     "get_auth_service",
     "get_user_service",
+    "get_chat_service",
+    "get_message_service",
     "get_ai_service",
 ]
