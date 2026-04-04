d9295aa944a3fce786d0f669a0abf37ec223b9ac
ScPro <muhamadievbogdan@gmail.com>
2026-04-04 05:02:58 +0500

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import { Home } from "./pages/Home";
+import { Auth } from "./pages/Auth"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useThemeInit } from './hooks/useTheme';
+import { useAuthInit } from "./hooks/useAuth";
+
+function AppContent() {
+    //useAuthInit(); 
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+        </Routes>
+    );
+}
 
 function App() {
     useThemeInit();
     return (
         <>
-            <Home></Home>
+            <BrowserRouter>
+                <AppContent/>
+            </BrowserRouter>
         </>
     );
 }
